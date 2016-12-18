91e97f6fa43c9e736b8f978e37b2b8f4ab2cbaa0
Joey <Joey@192.168.1.35>
2016-12-18 20:41:09 +0000

    Subiendo cuerpo principal del programa

--- src/main/java/quicksort/QuickSort/App.java
+++ src/main/java/quicksort/QuickSort/App.java
@@ -6,6 +6,22 @@ package quicksort.QuickSort;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		int[] v = new int[10];
+		
+		v[0] = 7;
+		v[1] = 6;
+		v[2] = 2;
+		v[3] = 1;
+		v[4] = 9;
+		v[5] = 4;
+		v[6] = 5;
+		v[7] = 3;
+		v[8] = 8;
+		v[9] = 0;
+		
+		Quicksort Q = new Quicksort();
+		Q.print(v);
+		Q.sort(v);
+		Q.print(v);
 	}
 }
